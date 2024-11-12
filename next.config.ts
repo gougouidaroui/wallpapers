0fbb010d7a8219b259c7748884cde4b296c19a10
yoonhaw <bbabrehamza@gmail.com>
2024-11-12 18:13:18 +0100

    fixed some resolve issues from the utfs site

--- next.config.ts
+++ next.config.ts
@@ -3,16 +3,15 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   /* config options here */
     images: {
-        domains: ['utfs.io'],
         remotePatterns: [
             {
                 protocol: "https",
                 hostname: "utfs.io",
                 port: "",
-                pathname: "/a/y0s1o6y4oe/**",
+                pathname: "/**",
             },
         ],
-    }
+    },
 };
 
 export default nextConfig;
