6ac1ac4c30c2eb7b3c7ec75522c839a76593397d
yoonhaw <bbabrehamza@gmail.com>
2026-05-02 20:50:26 +0100

    maybe

--- src/lib/firebaseconfig.js
+++ src/lib/firebaseconfig.js
@@ -2,7 +2,9 @@ import { initializeApp, getApps } from "firebase/app";
 import { getFirestore } from "firebase/firestore";
 import { getAuth } from "firebase/auth";
 
+
 const firebaseConfig = JSON.parse(process.env.NEXT_PUBLIC_FIREBASE_CONFIG || '{}');
+console.log(firebaseConfig);
 
 const app = getApps().length > 0 ? getApps()[0] : initializeApp(firebaseConfig);
 export const db = getFirestore(app);
