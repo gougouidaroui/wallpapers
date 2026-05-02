a6c98a43f63d2e43892d4d77c931393f73f7ed0c
yoonhaw <bbabrehamza@gmail.com>
2026-05-02 20:24:21 +0100

    removed dummy key

--- src/lib/firebaseconfig.js
+++ src/lib/firebaseconfig.js
@@ -3,7 +3,7 @@ import { getFirestore } from "firebase/firestore";
 import { getAuth } from "firebase/auth";
 
 const firebaseConfig = {
-    apiKey: process.env.NEXT_PUBLIC_API_KEY || "dummy-key-for-build",
+    apiKey: process.env.NEXT_PUBLIC_API_KEY,
     authDomain: process.env.NEXT_PUBLIC_AUTH_DOMAIN,
     projectId: process.env.NEXT_PUBLIC_PROJECT_ID,
     storageBucket: process.env.NEXT_PUBLIC_STORAGE_BUCKET,
